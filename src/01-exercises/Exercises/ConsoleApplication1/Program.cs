e4539ba5c1ebf10c05050dbef86c399e96b112e3
ARaie <annika_875@hotmail.com>
2017-10-23 14:00:39 +0300

    Add HelloWorld console application

---
+++ src/01-exercises/Exercises/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 01-Hello-World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Press any line to continue...");
+        }
+    }
+}
