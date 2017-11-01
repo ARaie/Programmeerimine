18651e11f51f0ecc6fe43528cdd0f7696e4bbc67
Annika Raie <annika.raie@profitsoftware.com>
2017-11-01 12:09:05 +0200

    Kodutöö2

--- src/02-exercises/DiceRoller/DiceRoller/DiceRollerCommandParser.cs
+++ src/02-exercises/DiceRoller/DiceRoller/DiceRollerCommandParser.cs
@@ -13,7 +13,72 @@ namespace DiceRoller
        
         public List<Dice> Source(string input)
         {
+
+            List<Dice> korrad = new List<Dice>();
+
             var dices = new List<Dice>();
+
+            string täringud = input;
+            string[] täring = täringud.Split(' ');
+
+            foreach (var osa in täring)
+            {
+
+                var a = osa.Split('d')[0];
+                int kordused = Int32.Parse(a); 
+                var täringuSuurus = osa.Split('d')[1];
+                korrad.Add(new Dice(kordused, täringuSuurus));
+                
+            }
+            foreach (var kord in korrad)
+            {
+                if (kord.Description == "6")
+                {
+                    for (int i = 0; i < kord.Sides; i++)
+                    {
+                        dices.Add(Dice.D6);
+                    }
+                     
+                }
+                else if (kord.Description == "8")
+                {
+                    for (int i = 0; i < kord.Sides; i++)
+                    {
+                        dices.Add(Dice.D8);
+                    }
+                }
+                else if (kord.Description == "20")
+                {
+                    for (int i = 0; i < kord.Sides; i++)
+                    {
+                        dices.Add(Dice.D20);
+                    }
+                }
+               else if (kord.Description == "4")
+                {
+                    for (int i = 0; i < kord.Sides; i++)
+                    {
+                        dices.Add(Dice.D4);
+                    }
+                }
+                else if (kord.Description == "12")
+                {
+                    for (int i = 0; i < kord.Sides; i++)
+                    {
+                        dices.Add(Dice.D12);
+                    }
+                }
+                else if (kord.Description == "10")
+                {
+                    for (int i = 0; i < kord.Sides; i++)
+                    {
+                        dices.Add(Dice.D10);
+                    }
+                }
+            }
+
+
+            /*
 
             if (input == "/roll 3d6 2d8")
             {
@@ -55,7 +120,7 @@ namespace DiceRoller
                 dices.Add(Dice.D12);
                 
 
-            }
+            }*/
 
             return dices;
         }
