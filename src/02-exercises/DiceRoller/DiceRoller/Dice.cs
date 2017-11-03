bd3ca9dec6fcdd903610332a061b4df21fde2156
Annika Raie <annika.raie@profitsoftware.com>
2017-11-03 11:19:48 +0200

    parandus

--- src/02-exercises/DiceRoller/DiceRoller/Dice.cs
+++ src/02-exercises/DiceRoller/DiceRoller/Dice.cs
@@ -24,17 +24,17 @@ namespace DiceRoller
             this.Sides = sides;
 
         }
-        //public static Dice D6 = new Dice(6, "1d6");
+        public static Dice D6 = new Dice(6, "1d6");
  
-         //public static Dice D8 = new Dice(8, "1d8");
+         public static Dice D8 = new Dice(8, "1d8");
  
-         //public static Dice D20 = new Dice(20, "1d20");
+         public static Dice D20 = new Dice(20, "1d20");
  
-         //public static Dice D10 = new Dice(10, "1d10");
+         public static Dice D10 = new Dice(10, "1d10");
  
-         //public static Dice D4 = new Dice(4, "1d4");
+         public static Dice D4 = new Dice(4, "1d4");
  
-         //public static Dice d = new Dice(12, "1d12");
+         public static Dice D12 = new Dice(12, "1d12");
  
 
         public static Dice FromString(string kirjeldus)
@@ -43,27 +43,27 @@ namespace DiceRoller
 
             if (kirjeldus == "6")
             {
-                dice = new Dice(6, "1d6");
+                dice =  D6;
             }
             else if (kirjeldus == "4")
             {
-                dice = new Dice(4, "1d4");
+                dice = D4;
             }
             else if (kirjeldus == "8")
             {
-                dice = new Dice(8, "1d8");
+                dice = D8;
             }
             else if (kirjeldus == "10")
             {
-                dice = new Dice(10, "1d10");
+                dice = D10;
             }
             else if (kirjeldus == "12")
             {
-                dice = new Dice(12, "1d12");
+                dice = D12;
             }
             else if (kirjeldus == "20")
             {
-                dice = new Dice(20, "1d20");
+                dice = D20;
             }
             return dice;
     }
